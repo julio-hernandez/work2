cf1e7d2e17b9fa68f7d5e093deaaf9f18f5f932c
julio-hernandez <tenek75@gmail.com>
2018-05-09 14:21:28 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,7 +3,7 @@ class profile::agent_nodes{
   dockeragent::node{'web.puppet.vm':}
   dockeragent::node{'db.puppet.vm':}
   host {'web.puppet.vm':
-    ensure =>present.
+    ensure =>present,
     ip =>'172.18.0.3',
   }
   
